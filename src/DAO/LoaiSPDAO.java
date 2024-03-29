710d6f90bb0222e876b429d0235a6a94836d2dfa
Phuociter <128914255+Phuociter@users.noreply.github.com>
2024-03-29 12:51:22 +0700

    Update LoaiSPDAO.java

--- src/DAO/LoaiSPDAO.java
+++ src/DAO/LoaiSPDAO.java
@@ -1,13 +1,87 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
+import DTO.LoaiSP;
 
-/**
- *
- * @author nguye
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class LoaiSPDAO {
     
+    public ArrayList<LoaiSP> getLoaiSanPham(){
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String url = "jdbc:sqlserver://DESKTOP-P\\SQLPHUOCD:1433;databaseName=quanlythucannhanh;encrypt = true;trustServerCertificate = true";
+            String userName = "sa";
+            String passWord = "11111111";
+
+            Connection connection = DriverManager.getConnection(url, userName, passWord);
+            String slq = "select * from loaiSP";
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(slq);
+            ArrayList<LoaiSP> listLoaiSP = new ArrayList<>();
+            while (rs.next()) {
+                LoaiSP Loaisp = new LoaiSP();
+                Loaisp.setMaLoai(rs.getInt(1));
+                Loaisp.setTenLoai(rs.getString(2));
+                listLoaiSP.add(Loaisp);
+            }
+            return listLoaiSP;
+        }catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public boolean updateInfoLoaiSP(int loaiSP, String tenLoai) {
+        boolean ketqua = false;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String url = "jdbc:sqlserver://DESKTOP-P\\SQLPHUOCD:1433;databaseName=quanlythucannhanh;encrypt = true;trustServerCertificate = true";
+            String userName = "sa";
+            String passWord = "11111111";
+
+            Connection connection = DriverManager.getConnection(url, userName, passWord);
+            String sql = "update loaiSP set tenLoai=? where maLoai=?";
+            PreparedStatement pre = connection.prepareStatement(sql);
+            pre.setString(1, tenLoai);
+            pre.setInt(2, loaiSP);
+            ketqua = pre.executeUpdate() > 0;
+            return ketqua;
+        } catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean themLoaiSanPham(LoaiSP loaiSP){
+        boolean ketqua = false;
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String url = "jdbc:sqlserver://DESKTOP-P\\SQLPHUOCD:1433;databaseName=quanlythucannhanh;encrypt = true;trustServerCertificate = true";
+            String userName = "sa";
+            String passWord = "11111111";
+
+            Connection connection = DriverManager.getConnection(url, userName, passWord);
+            String sql = "insert into loaiSP(maLoai, tenLoai)  values(?, ?)";
+            PreparedStatement pre = connection.prepareStatement(sql);
+            pre.setInt(1,loaiSP.getMaLoai());
+            pre.setString(2, loaiSP.getTenLoai());
+            
+            pre.execute();
+            ketqua = pre.executeUpdate() > 1;
+
+        }catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return ketqua;
+    }
+
 }
