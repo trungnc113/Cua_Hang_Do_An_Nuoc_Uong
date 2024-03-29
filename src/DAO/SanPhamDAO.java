7f0bf67a12265d723921e68f8991c028a71839ea
Phuociter <128914255+Phuociter@users.noreply.github.com>
2024-03-29 12:51:01 +0700

    Update SanPhamDAO.java

--- src/DAO/SanPhamDAO.java
+++ src/DAO/SanPhamDAO.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package DAO;
+package DTO;
 
-/**
- *
- * @author nguye
- */
-public class SanPhamDAO {
-    
+public class SanPham {
+    private int maSP;
+    private String tenSP;
+    private int maLoai;
+    private int soLuong;
+    private String donViTinh;
+    private String hinhAnh;
+    private int donGia;
+    private int trangThai;
+
+    public SanPham() {
+
+    }
+
+    public SanPham(int maSP, String tenSP, int maLoai, int soLuong, String donViTinh, String hinhAnh, int donGia,int trangThai) {
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.maLoai = maLoai;
+        this.soLuong = soLuong;
+        this.donViTinh = donViTinh;
+        this.hinhAnh = hinhAnh;
+        this.donGia = donGia;
+        this.trangThai=trangThai;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public int getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(int maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public int getMaLoai() {
+        return maLoai;
+    }
+
+    public void setMaLoai(int maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+
+    public String getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(String hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+
+    public int getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
 }
